4152f1ca977577f7ca03d24952a4ab63d91ac5cd
hzy <andrew_hzy@163.com>
2023-03-09 20:41:50 +0800

    issue #298 : Andrew : add trylock for concurrency control

--- merge/dbmetrics/dbmetrics.go
+++ merge/dbmetrics/dbmetrics.go
@@ -1,6 +1,7 @@
 package dbmetrics
 
 import (
+	"math"
 	"sync"
 )
 
@@ -15,16 +16,50 @@ type (
 )
 
 var (
-	once sync.Once
-	dbm  dbMetrics
+	once   sync.Once
+	dbm    dbMetrics
+	locks  []sync.RWMutex
+	n, mod int
 )
 
-func Init()                    { once.Do(func() { dbm = make(dbMetrics) }) }
+func Init() { initiate(8) }
+func initiate(lockNum int) {
+	once.Do(func() {
+		dbm = make(dbMetrics)
+		n = 1 << int(math.Log2(float64(lockNum)))
+		mod = n - 1
+		locks = make([]sync.RWMutex, n)
+		for i := 0; i < n; i++ {
+			locks[i] = sync.RWMutex{}
+		}
+	})
+}
 func GetZeroMetrics() *Metrics { return &Metrics{0, 0, 0, 0} }
 
-func DeleteMetrics(fd int)                   { delete(dbm, int32(fd)) }
-func PutMetrics(fd int, m *Metrics)          { dbm[int32(fd)] = *m }
-func GetMetrics(fd int) (m Metrics, ok bool) { m, ok = dbm[int32(fd)]; return }
+func DeleteMetrics(fd int) {
+	for !locks[fd&mod].TryLock() {
+	}
+	delete(dbm, int32(fd))
+	locks[fd&mod].Unlock()
+
+}
+
+func PutMetrics(fd int, m *Metrics) {
+	for !locks[fd&mod].TryLock() {
+	}
+	dbm[int32(fd)] = *m
+	locks[fd&mod].Unlock()
+
+}
+
+func GetMetrics(fd int) (m Metrics, ok bool) {
+	for !locks[fd&mod].TryRLock() {
+	}
+	m, ok = dbm[int32(fd)]
+	locks[fd&mod].RUnlock()
+
+	return
+}
 
 func (m *Metrics) UpdateValid(entriesChange, bytesChange int) {
 	m.validEntries += int32(entriesChange)
